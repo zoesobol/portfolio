f81b4adfc1e7ee63111d16f4ee83fbd5070672d8
Zoe Sobol <zoesobol98@gmail.com>
2021-03-04 00:12:35 -0300

    I think I finally fixed it

--- blog/forms.py
+++ blog/forms.py
@@ -3,26 +3,26 @@ from .models import Post, Category, Comment
 
 #para generar el dropdown de las categorías
 
-def choice_list():
+"""def choice_list():
 	choices = Category.objects.all().values_list('name','name')
 
 	choice_list = []
 	for item in choices:
 		choice_list.append(item)
 	
-	return choice_list
-
+	return choice_list"""
 
 
 class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ('title', 'author', 'category', 'body', 'header_image')
+		category = forms.ModelChoiceField(queryset=Category.objects.all(), widget=forms.SelectMultiple, required=False)
 
 		widgets = {
 			'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Insert title'}),
 			'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'elder', 'type':'hidden'}),
-			'category': forms.Select(choices=choice_list(), attrs={'class': 'form-select'}),
+			#'category': forms.Select(queryset=Category.objects.all(), attrs={'class': 'form-select'}),
 			'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Insert text'}),
 
 		}
